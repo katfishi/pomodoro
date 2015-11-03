adfc71ed64191bb5ea68a4a740148d86c52f1426
Katheryn Shi <kshi@coursera.org>
2015-11-02 19:59:51 -0800

    Pomodoro title

--- index.ios.js
+++ index.ios.js
@@ -16,15 +16,8 @@ var Pomodoro = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.title}>
+          Pomodoro
         </Text>
       </View>
     );
@@ -32,22 +25,18 @@ var Pomodoro = React.createClass({
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#740000',
+      padding: 30,
+    },
+    title: {
+      color: 'white',
+      fontSize: 65,
+      marginBottom: 75,
+    },
 });
 
 AppRegistry.registerComponent('Pomodoro', () => Pomodoro);
