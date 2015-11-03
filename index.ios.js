73569014f739eb8e42093b44cbee407f20cf0c4a
Katheryn Shi <kshi@coursera.org>
2015-11-02 20:03:42 -0800

    getInitialState, timer

--- index.ios.js
+++ index.ios.js
@@ -13,11 +13,25 @@ var {
 } = React;
 
 var Pomodoro = React.createClass({
+  getInitialState: function() {
+    return {
+      min: 25,
+      sec: 0,
+    };
+  },
+
+  formatNum: function(num) {
+    return num < 10 ? '0' + num : num;
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           Pomodoro
+        </Text>
+        <Text style={styles.timer}>
+          {this.formatNum(this.state.min)}:{this.formatNum(this.state.sec)}
         </Text>
       </View>
     );
@@ -35,6 +49,12 @@ var styles = StyleSheet.create({
     title: {
       color: 'white',
       fontSize: 65,
+      marginBottom: 75,
+    },
+    timer: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 48,
       marginBottom: 75,
     },
 });
